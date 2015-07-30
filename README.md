90834ab680225f35100497fe6c6d7ffae5b857f7
tanmayAccela <tthakur@accela.com>
2015-07-29 21:23:18 -0700

    Installed django and started project

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # django_analytics
 django site for analytics of massive data
+
+Notes for me:
+```
+$virtualenv djangovenv
+```
---
+++ djangovenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/tthakur/Documents/github_workspace/tanmaythakur/django_analytics/djangovenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
